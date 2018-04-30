f2c7cc86a07e20c46be5f54fc81bb872933aac1d
Mads Kristensen <post@madskristensen.net>
2018-04-30 14:16:35 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Solution Load sample
+
+[![Build status](https://ci.appveyor.com/api/projects/status/khf234gs5mg2xp72?svg=true)](https://ci.appveyor.com/project/madskristensen/singlefilegeneratorsample)
+
+**Applies to Visual Studio 2015 and newer**
+
+This example shows how to listen to solution events when the package might not be initialized until after the solution has loaded.
+
+Clone the repo to test out the sample in Visual Studio 2017 yourself.
+
+## What are solution load events?
+Description coming soon...
+
+[See full Pakcage class in the source](src/VSPackage.cs)
+
+
+
+## License
+[Apache 2.0](LICENSE)
---
+++ src/VSPackage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Events;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace SolutionLoadSample
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [Guid("61eadc52-5677-4548-b273-08f1e6574f71")]
+    public sealed class VSPackage : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            if (await IsSolutionLoadedAsync())
+            {
+                HandleOpenSolution();
+            }
+
+            SolutionEvents.OnAfterOpenSolution += HandleOpenSolution;
+        }
+
+        private async Task<bool> IsSolutionLoadedAsync()
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
+            var solService = await GetServiceAsync(typeof(IVsSolution)) as IVsSolution;
+
+            ErrorHandler.ThrowOnFailure(solService.GetProperty((int)__VSPROPID.VSPROPID_IsSolutionOpen, out object value));
+
+            return value is bool isSolOpen && isSolOpen;
+        }
+
+        private void HandleOpenSolution(object sender = null, EventArgs e = null)
+        {
+            // Handle the open solution
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            SolutionEvents.OnAfterBackgroundSolutionLoadComplete -= HandleOpenSolution;
+            base.Dispose(disposing);
+        }
+    }
+}
