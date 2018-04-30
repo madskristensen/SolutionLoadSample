cc6502dfcc5c83b2c4377af73a89a05d24093523
Mads Kristensen <post@madskristensen.net>
2018-04-30 14:24:38 -0700

    Added code comments

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -10,20 +10,26 @@ using Task = System.Threading.Tasks.Task;
 
 namespace SolutionLoadSample
 {
+    [Guid("61eadc52-5677-4548-b273-08f1e6574f71")]
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    // The following lines will schedule the package to be initialized when a solution has loaded
     [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string, PackageAutoLoadFlags.BackgroundLoad)]
-    [Guid("61eadc52-5677-4548-b273-08f1e6574f71")]
     public sealed class VSPackage : AsyncPackage
     {
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            if (await IsSolutionLoadedAsync())
+            // Since this package might not be initialized until after a solution has loaded,
+            // we need to check if a solution has already been loaded and then handle it.
+            bool isSolutionLoaded = await IsSolutionLoadedAsync();
+
+            if (isSolutionLoaded)
             {
                 HandleOpenSolution();
             }
 
+            // Listen for subsequent solution events
             SolutionEvents.OnAfterOpenSolution += HandleOpenSolution;
         }
 
@@ -39,13 +45,8 @@ namespace SolutionLoadSample
 
         private void HandleOpenSolution(object sender = null, EventArgs e = null)
         {
-            // Handle the open solution
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            SolutionEvents.OnAfterBackgroundSolutionLoadComplete -= HandleOpenSolution;
-            base.Dispose(disposing);
+            // Handle the open solution and try to do as much work
+            // on a background thread if possible
         }
     }
 }
